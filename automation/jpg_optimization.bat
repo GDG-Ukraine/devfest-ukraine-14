c95a06f752731558dabc8e9c68779c50acb57d83
Oleh Zasadnyy <oleh.zasadnyy@gmail.com>
2014-07-25 17:05:33 +0300

    Optimized images. Batch scripts for automation of images and minifying on .css and .js

--- automation/jpg_optimization.bat
+++
@@ -1,4 +0,0 @@
-@echo none
-REM Optimizing JPEG with jpegtran
-forfiles /p %1 /s /m "*.jpg" /c "cmd /c  echo processing @path && %~dp0%\jpegtran.exe -optimize -progressive -copy none -outfile @path @path"
-pause
--- automation/jpg_png_optimization.bat
+++
@@ -1,8 +0,0 @@
-@echo none
-REM Optimizing JPEG with jpegtran
-forfiles /p %1 /s /m "*.jpg" /c "cmd /c  echo processing @path && %~dp0%\jpegtran.exe -optimize -progressive -copy none -outfile @path @path"
-REM Optimizing PNG with pngout
-forfiles /p %1 /s /m "*.png" /c "cmd /c  echo processing @path && %~dp0%\pngout.exe @path"
-REM Optimizing PNG with optipng
-rem forfiles /p %1 /s /m "*.png" /c "cmd /c  echo processing @path && %~dp0%\optipng.exe -force -o7 @path"
-pause

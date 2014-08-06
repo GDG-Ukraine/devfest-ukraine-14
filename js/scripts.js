021e6b2871f3f5da3804e2f30336b98819f09dfa
Oleh Zasadnyy <oleh.zasadnyy@gmail.com>
2014-08-07 00:34:29 +0300

    Optimized partnership proposition, reduced header size, links work on buttons

--- js/scripts.js
+++ js/scripts.js
@@ -13,6 +13,7 @@
             }
             $('body').css('overflow', 'auto');
             hidePreloader();
+            initGooglePlus();
             generateSameHeight();
         });
 
@@ -265,14 +266,14 @@
 
 
     //Google plus
-    (function() {
+    function initGooglePlus() {
         var po = document.createElement('script');
         po.type = 'text/javascript';
         po.async = true;
         po.src = 'https://apis.google.com/js/platform.js';
         var s = document.getElementsByTagName('script')[0];
         s.parentNode.insertBefore(po, s);
-    })();
+    }
 
     //Google maps
     if (typeof googleMaps !== 'undefined') {

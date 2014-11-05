ab002f83fc1de4680dbce96d0e3f48cb4f76cc1d
Oleh Zasadnyy <oleh.zasadnyy@gmail.com>
2014-11-05 17:04:48 +0200

    Stop video after  closing modal window

--- js/scripts.js
+++ js/scripts.js
@@ -265,6 +265,10 @@
                 $(this).parent().addClass('hidden-xs');
             }
         });
+        $('.modal').on('hidden.bs.modal', function () {
+            var iframe = $(this).find('iframe');
+            iframe.attr('src', iframe.attr('src'));
+        });
 
 
         if (typeof twitterFeedUrl !== 'undefined') {
